2c35838b0bc3bf077587e98a1fe3ef005aeffc78
melia miller <meliajm@gmail.com>
2019-11-26 04:35:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -111,7 +111,7 @@ def movies_with_directors_set(source)
   while index < source.length do 
     index2 = 0 
     while index2 < source[index].length do
-      new_array << movies_with_director_key(source[index][:name], source[index][:movies][index2])
+      new_array << movies_with_director_key(source[index][:name], source[index][:movies][index2][:title])
     index2 += 1
     end
   index += 1
